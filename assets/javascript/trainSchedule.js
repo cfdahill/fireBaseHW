d105dd7415032a7ae5fed3921d6cca5c8764cc65
Chay Dahill <cfdahill@gmail.com>
2018-03-16 14:01:02 -0700

    currently autoupdating, need to make remove/edit work

--- assets/javascript/trainSchedule.js
+++ assets/javascript/trainSchedule.js
@@ -15,30 +15,27 @@ var name = "";
 var destination = "";
 var frequency = 0; //will be measured in minutes
 var firstArrival = "";
-var td = $("<td>");
-var tr = $("<tr>");
+var tableHeader = "<tr><th>Train</th><th>Destination</th><th>Time of Next Arrival</th><th>Minutes Away</th><th>Frequency (in minutes)</th><th>First Arrival</th></tr>";
 var timeTillNext;
 var minutesTillNext;
 
-function nextArrival(start, frequency) {
-    var first = moment(start, "HH:mm a").format();
-    var currentTime = moment().format();
-    var timeSince = moment().diff(moment(first), "minutes"); //time between now and first train
-    minutesTillNext = frequency - (timeSince%frequency);
-    timeTillNext = moment().add(minutesTillNext, "minutes").format("h:mm a");
-
-    console.log("First train: " +first);
-    console.log("Current time: " + currentTime);
-    console.log("Minutes since first train: " +timeSince);
-    console.log("Minutes till next train: " + minutesTillNext);
-    console.log("Time of next train: " + timeTillNext);
-}
-
 $(document).ready(function () {
-    function makeRow(child) {
-        $("#schedule").append("<tr data-train: '" + child.name + "'><td>" + child.name + "</td><td>" + child.destination + "</td><td>" + timeTillNext + "</td><td>" + minutesTillNext + "</td><td>" + child.frequency + "</td><td>" + child.firstArrival + "</td></tr>");
+    //nextArrival will determine when the next arrival will be in terms of the time and minutes away
+    function nextArrival(start, frequency) {
+        var first = moment(start, "HH:mm a").format();
+        var currentTime = moment().format();
+        var timeSince = moment().diff(moment(first), "minutes"); //time between now and first train
+        minutesTillNext = frequency - (timeSince % frequency);
+        timeTillNext = moment().add(minutesTillNext, "minutes").format("h:mm a");
     }
 
+    //makeRow takes the train information and puts it all into a row for the schedule table
+    function makeRow(child) {
+        $("#schedule").append("<tr data-train: '" + child.name + "'><td>" + child.name + "</td><td>" + child.destination + "</td><td>" + timeTillNext + "</td><td>" + minutesTillNext + "</td><td>" + child.frequency + "</td><td>" + child.firstArrival + "</td><td><button type='submit' class='remove'>remove</button></td></tr>");
+        $(".remove").click(remove);
+    }
+
+    //Lets user put in new train information
     $("#submit").click(function (event) {
         event.preventDefault();
         name = $("#name").val().trim();
@@ -55,16 +52,29 @@ $(document).ready(function () {
         });
     });
 
-    database.ref().on("child_added", function (childSnapshot) {
-        var child = childSnapshot.val();
-        console.log(child.name);
-        /*console.log(child.destination);
-        console.log(child.frequency);
-        console.log(child.firstArrival);
-        console.log(child.dateAdded);*/
-        nextArrival(child.firstArrival, child.frequency);
-        makeRow(child);
-    });
+    //Delete train data from firebase and table:        
+    function remove(event) {
+        event.preventDefault();
+        var row = $(this).closest('tr');
+        row.remove();
+        //var rowId = row.attr('data-train');  THIS LINE AN ATTEMPT TO MAKE FOLLOWING LINE WORK
+      //  database.ref().child().remove()  THIS LINE CURRENTLY NOT WORKING
+    };
+
+    //Puts all of the trains information into the table
+    function trainSchedule() {
+        $("#schedule").empty();  //empties the table of previous information (needed for refreshing data)
+        $("#schedule").append(tableHeader); //adds the header of the table back
+        console.log("updated");
+        database.ref().on("child_added", function (childSnapshot) {
+            var child = childSnapshot.val();
+
+            nextArrival(child.firstArrival, child.frequency);  //does the calculations needed to get next arrival information
+            makeRow(child);  //puts all of the information for a train into a row
+        });
+    }
+    trainSchedule(); //runs on load up
+    setInterval(trainSchedule, 60000);  //refreshes every minute
 });
 /*
 Need to be able to push the following to fireBase:
